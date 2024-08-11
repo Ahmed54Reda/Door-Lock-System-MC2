1cf890699c9c9ada2cd46b5babfb05aa9c1ef7c0
Ahmed Reda Younis <ahmed54reda@gmail.com>
2024-08-11 19:22:05 +0300

    Add files via upload

---
+++ buzzer.c
@@ -0,0 +1,36 @@
+
+#include "buzzer.h"
+
+#include "gpio.h"
+
+
+/*
+ * Description :
+ * Initialize the Buzzer:
+ */
+void Buzzer_init(void)
+{
+	GPIO_setupPinDirection(BUZZER_PORT_ID,BUZZER_PIN_ID,PIN_OUTPUT); //set direction as output
+
+	GPIO_writePin(BUZZER_PORT_ID,BUZZER_PIN_ID,LOGIC_LOW); //set to LOW initially
+}
+
+
+/*
+ * Description :
+ * Turn ON Buzzer
+ */
+void Buzzer_on(void)
+{
+	GPIO_writePin(BUZZER_PORT_ID,BUZZER_PIN_ID,LOGIC_HIGH); //Set to HIGH
+}
+
+/*
+ * Description :
+ * Turn OFF Buzzer
+ */
+void Buzzer_off(void)
+{
+	GPIO_writePin(BUZZER_PORT_ID,BUZZER_PIN_ID,LOGIC_LOW); //set to LOW
+}
+
